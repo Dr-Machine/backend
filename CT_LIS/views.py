5a0a290e15c8a2303f6bf03a084096e24551a64a
Parsa <parsa.esfahanian.7596@gmail.com>
2021-07-03 12:33:20 +0430

    overhull on CT_LIS API done

--- CT_LIS/views.py
+++ CT_LIS/views.py
@@ -1,28 +1,47 @@
+import logging
+
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
-
 from CT_LIS.models import CTLungInfectionSegmentation
 from CT_LIS.serializers import CTLungInfectionSegmentationSerializers
+from CT_LIS.tasks import execute_run_model
+
+logger = logging.getLogger('backend')
 
 
 class CTLungInfectionSegmentationView(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
-    # parser_classes = [FileUploadParser]
-    serializer_class = CTLungInfectionSegmentationSerializers
     queryset = CTLungInfectionSegmentation.objects.all()
+    lookup_url_kwarg = 'id'
 
-    # lookup_url_kwarg = 'id'
+    def get_serializer_class(self):
+        return CTLungInfectionSegmentationSerializers
 
-    # def get_serializer_class(self):
-    #     return CTLungInfectionSegmentationSerializers
+    def filter_queryset(self, queryset):
+        queryset = self.get_queryset().filter(prescriber=self.request.user)
+        return queryset
 
-    # def filter_queryset(self, queryset):
-    #     queryset = self.get_queryset().filter(prescriber=self.request.user)
-    #     return queryset
+    @action(detail=True, methods=['GET'])
+    def run(self, request, *args, **kwargs):
+        case_object = self.get_object()
 
-    # def pre_save(self, obj):
-    #     obj.prescriber = self.request.user
-    #     obj.file = self.request.FILES.get('file')
+        try:
+            logger.info(f'Adding segmentation task to the queue...')
+            execute_run_model.delay(
+                case_directory_path=case_object.case_directory_path,
+                result_directory_path=case_object.result_directory_path,
+                id=case_object.id)
+            message = 'Segmentation task successfully added to the queue!'
+            logger.info(message)
+            data = {"detail": message}
+            return Response(data=data, status=status.HTTP_200_OK)
+
+        except Exception as e:
+            message = ("Failed to run CT Lung Infection Segmentation model. "
+                       f"Reason: {str(e)}.")
+            logger.error(message)
+            data = {"detail": message}
+            return Response(data=data, status=status.HTTP_400_BAD_REQUEST)
