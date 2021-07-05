b5fdf6044e9b01d8f762424f22a18c31b71a1d7f
Parsa <parsa.esfahanian.7596@gmail.com>
2021-07-05 18:22:58 +0430

    added ASCII logo to application startup

--- backend/wsgi.py
+++ backend/wsgi.py
@@ -5,3 +5,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
+
+with open('ascii.txt', 'r') as f:
+    ascii_logo = f.read()
+    print(ascii_logo)
--- manage.py
+++ manage.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python
-"""Django's command-line utility for administrative tasks."""
+"""
+Django's command-line utility for administrative tasks.
+"""
+
 import os
 import sys
 
 
 def main():
-    """Run administrative tasks."""
+    """
+    Run administrative tasks.
+    """
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -13,8 +18,7 @@ def main():
         raise ImportError(
             "Couldn't import Django. Are you sure it's installed and "
             "available on your PYTHONPATH environment variable? Did you "
-            "forget to activate a virtual environment?"
-        ) from exc
+            "forget to activate a virtual environment?") from exc
     execute_from_command_line(sys.argv)
 
 
